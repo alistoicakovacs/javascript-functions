0b39b728c46f9b09a7ad4f9a1b232da7aec7abc1
Ali Stoica Kovacs <78688064+alistoicakovacs@users.noreply.github.com>
2022-08-17 11:03:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const sumAll = function (number, ...numbers) {
   // first we create an array - total - here we pass in the numbers from the first number give as parameter, till the second number given as parameter.
   let total = [];
   // we create a for loop to find all the number from - to.
-  for(i=number;i<=numbers;i++) {
+  for(i=number; i<=numbers; i++) {
   // we push the numbers to the array
 	total.push(i)	
   }
